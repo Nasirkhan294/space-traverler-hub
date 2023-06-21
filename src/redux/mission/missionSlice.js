c8b204239b9af8a18d6562ea4c8120dfc0b95898
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-06-21 19:36:17 +0500

    Fetch missions data and render it into react component

--- src/redux/mission/missionSlice.js
+++ src/redux/mission/missionSlice.js
@@ -1,32 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchMissions = createAsyncThunk('missions/fetchMissions', async () => {
+  try {
+    const res = await axios.get('https://api.spacexdata.com/v3/missions');
+    return res.data;
+  } catch (error) {
+    throw new Error('Failed to fetch Missions');
+  }
+});
 
 const initialState = {
-  missions: [{
-    title: 'Thaicom',
-    description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-    status: 'not a member',
-    action: '',
-  },
-  {
-    title: 'telstar',
-    description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-    status: 'Active member',
-    action: '',
-  },
-  {
-    title: 'Jet 15',
-    description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-    status: 'not a member',
-    action: '',
-  },
-
-  ],
+  missions: [],
+  isLoading: false,
+  error: null,
 };
 
 const missionSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.pending, (state) => {
+        state.isLoading = true;
+      })
+
+      .addCase(fetchMissions.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.missions = action.payload;
+      })
+
+      .addCase(fetchMissions.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default missionSlice.reducer;
