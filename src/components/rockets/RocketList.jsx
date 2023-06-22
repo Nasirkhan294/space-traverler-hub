9e6f458dba22fe0ad5867fceedf0a772184b12c5
Sumeya <sumeyaibrahim72@gmail.com>
2023-06-22 07:16:36 +0300

    Fetch data from API

--- src/components/rockets/RocketList.jsx
+++ src/components/rockets/RocketList.jsx
@@ -1,11 +1,21 @@
 import { styled } from 'styled-components';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import Rockets from './Rockets';
+import { getRockets } from '../../redux/rockets/rocketSlice';
 
 export default function RocketList() {
-  const { rockets } = useSelector((store) => store.rocket);
+  const { rockets, isLoading } = useSelector((store) => store.rocket);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (rockets.length === 0) {
+      dispatch(getRockets());
+    }
+  }, []);
+
   return (
     <Div>
+      {isLoading === true ? <h3>Loading...</h3> : ''}
       {rockets.map((item) => <Rockets key={item.id} rockets={item} />)}
 
     </Div>
